83a9dcd9c341cdbf300185a6212baa41518d675a
lade0 <jesufemi33@yahoo.com>
2025-06-20 19:52:15 +0100

    initial

---
+++ main.py
@@ -0,0 +1,12 @@
+# create an ai chatbot that i promise won't take over the world
+print("hello i am roberto jr what's your name")
+name=input("enter your name")
+print("hellonice to meet ya",name)
+print("can you tell me why i smell")
+ans1=input("1.because you didn't shower,2.because bad people smell,3.you don't smell at all!")
+if ans1=="1":
+    print("if you say so")
+elif ans1=="2":
+    print("oh i've got a roast for you: you have something on your third chin")
+else:
+    print("sure some people like to hear lies to make them feel better but i don't stop trying to please me")
